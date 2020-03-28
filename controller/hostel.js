750d35b1abea3ef7a2866ef4646724f66cb06090
Thiti Dev <thiti.mwk.main@gmail.com>
2020-03-28 23:58:48 +0700

    [API]: Created uploadHostelPhoto

--- controller/hostel.js
+++ controller/hostel.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const ErrorResponse = require('../utils/errorResponse');
 
 const Hostel = require('../models/Hostel');
@@ -27,5 +28,54 @@ exports.createHostel = async (req, res, next) => {
 	res.status(201).json({
 		success: true,
 		data: created_hostel
+	});
+};
+
+// @desc    Update hostel photo
+// @route   PUT /api/hostels/:id/photo
+// @acess   Private
+exports.uploadHostelPhoto = async (req, res, next) => {
+	const hostel = await Hostel.findById(req.params.id); // findByIdAndDelete not gonna triggered the middle ware
+
+	if (!hostel) {
+		//return res.status(400).json({ success: false });
+		return next(new ErrorResponse(`Hostel not found with id of ${req.params.id}`, 404));
+	}
+
+	// Make sure user is the hostel's owner
+	if (hostel.owner.toString() !== req.user.id && req.user.role !== 'admin') {
+		return next(new ErrorResponse(`User ${req.user.id} is not authorized to update this hostel`, 401));
+	}
+
+	if (!req.files) {
+		return next(new ErrorResponse(`Please upload a file`, 400));
+	}
+
+	const file = req.files.file;
+
+	// Make sure the image is a photo
+	if (!file.mimetype.startsWith('image')) {
+		return next(new ErrorResponse(`Please upload an image file`, 400));
+	}
+
+	// Check filesize
+	if (file.size > process.env.MAX_FILE_UPLOAD) {
+		return next(new ErrorResponse(`Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`, 400));
+	}
+
+	// Create custom filename
+	file.name = `photo_${hostel._id}${path.parse(file.name).ext}`;
+
+	file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
+		if (err) {
+			console.log(err);
+			return next(new ErrorResponse(`Problem with file upload`, 500));
+		}
+
+		await Hostel.findByIdAndUpdate(req.params.id, {
+			photo: file.name
+		});
+
+		res.status(200).json({ success: true, data: file.name });
 	});
 };
--- routes/api/hostel.js
+++ routes/api/hostel.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getAllHostel, createHostel } = require('../../controller/hostel');
+const { getAllHostel, createHostel, uploadHostelPhoto } = require('../../controller/hostel');
 const { protect } = require('../../middleware/auth');
 router.route('/').get(getAllHostel).post(protect, createHostel);
+router.route('/:id/photo').put(protect, uploadHostelPhoto);
 
 module.exports = router;
