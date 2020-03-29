37edfc09a9a305c1fc5ca15f1ee579a4b964b3e4
Thiti Dev <thiti.mwk.main@gmail.com>
2020-03-29 14:53:18 +0700

    [function][cycle]: Created isStillAuthenticated check

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import cookie from 'react-cookies';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,6 +10,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // -------------- Redux -----------------
 import { Provider } from 'react-redux';
 import store from './redux/store';
+
+import { setCurrentUser } from './redux/actions/authActions';
 // --------------------------------------
 
 //
@@ -20,7 +23,34 @@ import Home from './components/Home';
 
 import PrivateRoute from './components/common/PrivateRoute';
 
+import DecodedJWT from './utils/DecodedJWT';
+
+const isStillAuthenticated = (token) => {
+	//Check for token
+	if (token) {
+		//Decode token and get user info and exp
+		const decoded = DecodedJWT(token);
+		// Set user and isAuthenticated
+		store.dispatch(setCurrentUser(decoded));
+
+		// Check for expired token
+		const currentTime = Date.now() / 1000;
+		if (decoded.exp < currentTime) {
+			//Logout User
+			//store.dispatch(logoutUser());
+			store.dispatch(setCurrentUser({})); // set to an empty object => isAuthenticated will be false
+			//Redirect to login
+			window.location.href = '/login';
+		}
+	}
+};
+
 class App extends React.Component {
+	componentDidMount() {
+		// later will decide if this has to do everytimes that the component is rendered or just once in this cycle
+		const token = cookie.load('token');
+		isStillAuthenticated(token);
+	}
 	render() {
 		//const state = store.getState();
 		return (
