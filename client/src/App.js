98169a02b82fdeeddc78ad2696e176a438acaaf6
Thiti Dev <thiti.mwk.main@gmail.com>
2020-04-01 22:04:40 +0700

    [global_styles]: Import react-tiny-fab bulit in css

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'hover.css/css/hover-min.css';
 import 'react-awesome-button/dist/styles.css';
+import 'react-tiny-fab/dist/styles.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // -------------- Redux -----------------
