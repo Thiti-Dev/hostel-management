faf58cbe70b3c5db9c73506eabdc155202fa1596
Thiti Dev <thiti.mwk.main@gmail.com>
2020-04-01 18:47:40 +0700

    [api][update]: getStatisticData :chart_with_upwards_trend:

--- controller/services.js
+++ controller/services.js
@@ -1,11 +1,16 @@
 const User = require('../models/User');
-
+const Booking = require('../models/Booking');
+const Hostel = require('../models/Hostel');
 exports.getStatisticData = async (req, res, next) => {
 	const userCount = await User.count();
+	const bookCount = await Booking.count();
+	const hostelCount = await Hostel.count();
 	res.status(200).json({
 		success: true,
 		data: {
-			total_user: userCount
+			total_user: userCount,
+			total_booking: bookCount,
+			total_hostel: hostelCount
 		}
 	});
 };
