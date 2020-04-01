f553b819c8b6f32981e6811b48af190a95fca25b
Thiti Dev <thiti.mwk.main@gmail.com>
2020-04-01 14:48:14 +0700

    [API]: Created getUserBooking (public)

--- controller/booking.js
+++ controller/booking.js
@@ -2,12 +2,12 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Hostel = require('../models/Hostel');
 const Booking = require('../models/Booking');
+const User = require('../models/User');
 // @desc    Book a hostel
 // @route   POST /api/hostels/:hostelId/booking
 // @acess   Private
 exports.bookHostel = asyncHandler(async (req, res, next) => {
 	const { hostelId } = req.params;
-
 	//
 	// ─── DEV ────────────────────────────────────────────────────────────────────────
 	//
@@ -20,6 +20,29 @@ exports.bookHostel = asyncHandler(async (req, res, next) => {
 	res.status(201).json({
 		success: true,
 		data: booked_hostel
+	});
+});
+
+// @desc    Get booking history of the given username
+// @route   POST /api/users/:username/booking
+// @acess   Public
+exports.getUserBooking = asyncHandler(async (req, res, next) => {
+	const { username } = req.params;
+	console.log(username);
+	const user = await User.findOne({ username });
+	if (!user) {
+		return next(new ErrorResponse(`Username ${username} is not exist in the database`, 404));
+	}
+	const user_id = user._id;
+
+	const booking_history = await Booking.find({ user: user_id }).sort('-createdAt').populate({
+		path: 'hostel',
+		select: 'photo name'
+	}); // sort => showing the lastest first;
+
+	res.status(200).json({
+		sucess: true,
+		data: booking_history
 	});
 });
 
--- routes/api/booking.js
+++ routes/api/booking.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router({ mergeParams: true }); // be able to be re-routed
 const { protect } = require('../../middleware/auth');
 
-const { bookHostel, getMyBooking } = require('../../controller/booking');
+const { bookHostel, getMyBooking, getUserBooking } = require('../../controller/booking');
 
-router.route('').post(protect, bookHostel);
+router.route('').post(protect, bookHostel).get(getUserBooking);
 router.route('/myBooking').get(protect, getMyBooking);
 module.exports = router;
