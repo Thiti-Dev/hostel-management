a2a16234b68bb84fd302c09af21aecfe5e5aeeb6
Thiti Dev <thiti.mwk.main@gmail.com>
2020-04-02 20:27:05 +0700

    edited port variable

--- server.js
+++ server.js
@@ -57,9 +57,9 @@ const mainThread = async () => {
 	app.use(errorHandler);
 	// ────────────────────────────────────────────────────────────────────────────────
 
-	const PORT = process.env.SERVER_PORT || 5000;
+	const PORT = process.env.PORT || 5000;
 
-	const server = app.listen(PORT || 5000, () => {
+	const server = app.listen(PORT, () => {
 		console.log(`The server is currently running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 	});
 
