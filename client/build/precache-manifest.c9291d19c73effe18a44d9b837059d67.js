2629a904b33c2d653f8d8d47c7fa4fab5d74b0d7
K3sz-X64 <36455825+Thiti-Dev@users.noreply.github.com>
2020-04-03 21:53:15 +0700

    :rotating_light: :rocket: @stable

---
+++ client/build/precache-manifest.c9291d19c73effe18a44d9b837059d67.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0c4d772c59c536b27aa0edf0a113c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f9aafe5aae3f442c223",
+    "url": "/static/css/2.193bb26f.chunk.css"
+  },
+  {
+    "revision": "c5af7abf76ca478a171b",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "6f9aafe5aae3f442c223",
+    "url": "/static/js/2.bc202135.chunk.js"
+  },
+  {
+    "revision": "e8ca77d9b1ad35762aca333f01fc5423",
+    "url": "/static/js/2.bc202135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5af7abf76ca478a171b",
+    "url": "/static/js/main.e122988a.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "3d59306d491d5e7e47849a2d0d41e432",
+    "url": "/static/media/Cat_Z.3d59306d.png"
+  },
+  {
+    "revision": "b617f7e5327d00b7f732717331287771",
+    "url": "/static/media/add.b617f7e5.png"
+  },
+  {
+    "revision": "5bce644e4905a810c8d301ab42d45233",
+    "url": "/static/media/checklist.5bce644e.png"
+  },
+  {
+    "revision": "7dc9a133de5eaf10c3415236fa2b3d35",
+    "url": "/static/media/edit-account.7dc9a133.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c85a8fd6c24a3ccaff476da5d157648f",
+    "url": "/static/media/partner.c85a8fd6.png"
+  },
+  {
+    "revision": "c2f4dc9aa95aa0d7684d1d629365ca90",
+    "url": "/static/media/verify.c2f4dc9a.png"
+  }
+]);
